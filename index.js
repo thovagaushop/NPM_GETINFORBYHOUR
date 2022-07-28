78a1a269ef891088d4b24c85cf3b440d5a6871f8
thovagaushop <minhmeomeomun@gmail.com>
2022-07-28 17:00:55 +0700

    2.0.1

--- index.js
+++ index.js
@@ -2,25 +2,29 @@ const moment = require('moment')
 
 
 //Hàm thống kê theo giờ
-function getInformationByHour(data, type_time, array_type_name) {
+function getInformationByHour(data, type_time, array_type_name, total_amount) {
 
     //Length status cần thống kê
-    var number_fields = array_type_name.length
+    let number_fields = array_type_name.length
 
-    var defaultArr = []
+    let defaultArr = []
 
-    for(var i = 0; i < number_fields; i++) {
+    for(let i = 0; i < number_fields; i++) {
         defaultArr.push(0)
     }
 
-    //Mảng trả về
-    var result = []
+    if (total_amount === true) {
+        defaultArr.push(parseFloat(0))
+    }
 
-    for(var i = 0; i < 24; i++) {
-        var arr = []
-        var index = ""
+    //Mảng trả về
+    let result = []
+
+    for(let i = 0; i < 24; i++) {
+        let arr = []
+        let index = ""
         if (type_time === 12) {
-            var time_tails = i >= 12 ? " pm" : " am"
+            let time_tails = i >= 12 ? " pm" : " am"
             index = ((i % type_time) || type_time).toString() + time_tails
         } else index = i.toString()
         arr.push(index)
@@ -33,23 +37,45 @@ function getInformationByHour(data, type_time, array_type_name) {
     //Check từng node trong data
     if (data.length) {
         data.forEach(element => {
-            if (element.node === undefined) return "Your array element must named : 'node'"
-            if (element.node.occurredAt === undefined) return "Your time field must named : 'occurredAt'"
-            if (element.node.type === undefined) return "Your status field must namd : 'type'"
+            if (element.node === undefined) {
+                console.log("Your array element must named : 'node'")
+                return null
+            } 
+            if (element.node.occurredAt === undefined) {
+                console.log("Your time field must named : 'occurredAt'")
+                return null
+            } 
+            if (element.node.type === undefined) {
+                console.log("Your status field must namd : 'type'");
+                return null
+            }
+
+            if (total_amount === true) {
+                if (element.node.charge.amount.amount === undefined) {
+                    console.log("Cannot find amount fields");
+                    return null
+                }
+            }
             //Lấy date time trong data
-            var date = moment(element.node.occurredAt)
+            let date = moment(element.node.occurredAt)
 
-            if (!date.isValid) return "Your time fields is not valid"
+            if (!date.isValid) {
+                console.log("Your time fields is not valid");
+                return null
+            } 
 
-            var hour = date.utc().hour()
+            let hour = date.utc().hour()
 
             //Push vào mảng
-            for (var i = 0; i < result.length; i++) {
+            for (let i = 0; i < result.length; i++) {
                 if (i === hour) {
-                    var statusIndex = array_type_name.findIndex(type => type === element.node.type)
+                    let statusIndex = array_type_name.findIndex(type => type === element.node.type)
                     
                     if (statusIndex !== -1) {
                         result[i][statusIndex + 1] += 1
+                        if (total_amount === true) {
+                            result[i][result[i].length - 1] += parseFloat(element.node.charge.amount.amount)
+                        }
                     }
                 }
             }
@@ -57,7 +83,69 @@ function getInformationByHour(data, type_time, array_type_name) {
 
         //Trả về mảng
         return result
-    } else return "Your data input must be an array!!!"
+    } else {
+        console.log("Your data input must be an array!!!");
+        return null
+    }
+
+}
+
+function getInformationByDay(data, array_type_name, total_amount) {
+    //Length status cần thống kê
+    let number_fields = array_type_name.length
+
+    //Mảng trả về
+    let result = []
+
+    for(let i = 0; i < number_fields; i++) {
+        result.push(0)
+    }
+
+    if (total_amount === true) {
+        result.push(parseFloat(0))
+    }
+
+    //Check từng node trong data
+    if (data.length) {
+        data.forEach(element => {
+            if (element.node === undefined) {
+                console.log("Your array element must named : 'node'")
+                return null
+            } 
+            if (element.node.occurredAt === undefined) {
+                console.log("Your time field must named : 'occurredAt'")
+                return null
+            } 
+            if (element.node.type === undefined) {
+                console.log("Your status field must namd : 'type'");
+                return null
+            }
+
+            if (total_amount === true) {
+                if (element.node.charge.amount.amount === undefined) {
+                    console.log("Cannot find amount fields");
+                    return null
+                }
+            }
+
+            //Push vào mảng
+            let statusIndex = array_type_name.findIndex(type => type === element.node.type)
+
+            if (statusIndex !== -1) {
+                result[statusIndex] += 1
+                if (total_amount === true) {
+                    result[result.length - 1] += parseFloat(element.node.charge.amount.amount)
+                }
+            }
+        })
+
+        //Trả về mảng
+        return result
+    } else {
+        console.log("Your data input must be an array!!!");
+        return null
+    }
 
 }
 module.exports.getInformationByHour = getInformationByHour;
+module.exports.getInformationByDay = getInformationByDay;
